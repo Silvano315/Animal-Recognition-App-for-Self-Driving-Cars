065043973d03918741b10dd6fccb7c958c47308c
Silvano Quarto <silvano.quarto@gmail.com>
2024-09-18 16:36:21 +0200

    models fx for visualization added

--- src/models.py
+++ src/models.py
@@ -1,8 +1,10 @@
 import pickle
 import numpy as np
+import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
 from plotly.subplots import make_subplots
+import matplotlib.pyplot as plt
 
 
 # Plot a choosen metric from history file
@@ -12,11 +14,6 @@ def scatter_plot_metrics(file_name):
         history = pickle.load(history_file)
 
     epochs = list(range(1, len(next(iter(history.values()))) + 1))
-
-    def calculate_mean_std(data):
-        mean = np.mean(data)
-        std = np.std(data)
-        return mean, std
 
     fig = go.Figure()
 
@@ -64,3 +61,118 @@ def scatter_plot_metrics(file_name):
     )
 
     fig.show()
+
+
+def plot_model_history(history):
+
+    fig = plt.figure(figsize=(12, 16))
+
+    mean_loss, std_loss = calculate_mean_std(history['loss'])
+    mean_val_loss, std_val_loss = calculate_mean_std(history['val_loss'])
+    plt.subplot(4, 2, 1)
+    plt.plot(history['loss'], label=f'Loss (mean: {mean_loss:.3f} ± {std_loss:.3f})')
+    plt.plot(history['val_loss'], label=f'Val Loss (mean: {mean_val_loss:.3f} ± {std_val_loss:.3f})')
+    plt.title('Loss Metric per epochs')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+
+    # Accuracy
+    mean_acc, std_acc = calculate_mean_std(history['accuracy'])
+    mean_val_acc, std_val_acc = calculate_mean_std(history['val_accuracy'])
+    plt.subplot(4, 2, 2)
+    plt.plot(history['accuracy'], label=f'Accuracy (mean: {mean_acc:.3f} ± {std_acc:.3f})')
+    plt.plot(history['val_accuracy'], label=f'Val Accuracy (mean: {mean_val_acc:.3f} ± {std_val_acc:.3f})')
+    plt.title('Accuracy Metric per epochs')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+
+    # Precision
+    mean_prec, std_prec = calculate_mean_std(history['precision'])
+    mean_val_prec, std_val_prec = calculate_mean_std(history['val_precision'])
+    plt.subplot(4, 2, 3)
+    plt.plot(history['precision'], label=f'Precision (mean: {mean_prec:.3f} ± {std_prec:.3f})')
+    plt.plot(history['val_precision'], label=f'Val Precision (mean: {mean_val_prec:.3f} ± {std_val_prec:.3f})')
+    plt.title('Precision Metric per epochs')
+    plt.xlabel('Epochs')
+    plt.ylabel('Precision')
+    plt.legend()
+
+    plt.tight_layout()
+    return fig
+
+
+# function to add a model name, description, performance to report.csv
+def add_to_report(model_name, metrics, description, report, file_path):
+    metrics_calculated = {}
+    for key, values in metrics.items():
+        mean, std = calculate_mean_std(values)
+        metrics_calculated[f'{key} Mean'] = mean
+        metrics_calculated[f'{key} Std'] = std
+
+    new_row = {
+        'Model': model_name,
+        'Description': description,
+        **metrics_calculated
+    }
+    new_row_df = pd.DataFrame([new_row])
+    report = pd.concat([report, new_row_df], ignore_index=True)
+    report.to_csv(file_path, index=False)
+
+
+def bar_plot_metric_perfomances(df_metric):
+
+    metrics = ['accuracy', 'loss', 'precision']
+    dropdown_buttons = []
+
+
+    fig = go.Figure()
+
+    for i, metric in enumerate(metrics):
+        fig.add_trace(go.Bar(
+        x=df_metric['Model'],
+        y=df_metric[f'{metric} Mean'].round(3),
+        error_y=dict(type='data', array=df_metric[f'{metric} Std'].round(3), visible=True),
+        name=f'Train {metric.capitalize()}',
+        marker_color='green',
+        visible=(i == 0) 
+        ))
+        fig.add_trace(go.Bar(
+        x=df_metric['Model'],
+        y=df_metric[f'val_{metric} Mean'].round(3),
+        error_y=dict(type='data', array=df_metric[f'val_{metric} Std'].round(3), visible=True),
+        name=f'Validation {metric.capitalize()}',
+        marker_color='coral',
+        visible=(i == 0) 
+        ))
+
+        dropdown_buttons.append({
+            'label': metric.capitalize(),
+            'method': 'update',
+            'args': [
+                {'visible': [False] * len(metrics) * 2},  
+                {'title': f'{metric.capitalize()} Comparison'}
+            ]
+        })
+        dropdown_buttons[-1]['args'][0]['visible'][i * 2] = True   
+        dropdown_buttons[-1]['args'][0]['visible'][i * 2 + 1] = True  
+
+    fig.update_layout(
+    title='Model Comparison: Accuracy, Loss, Precision',
+    xaxis_title='Model',
+    yaxis_title='Metric Value',
+    barmode='group', 
+    updatemenus=[{
+        'buttons': dropdown_buttons,
+        'direction': 'down',
+        'showactive': True,
+    }])
+
+    return fig
+
+
+def calculate_mean_std(data):
+        mean = np.mean(data)
+        std = np.std(data)
+        return mean, std
